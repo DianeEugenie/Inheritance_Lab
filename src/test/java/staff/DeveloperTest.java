b54c84be3c1afa749185fed691d844ef6a52bbff
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-30 16:44:42 +0100

    Sets up Developer Tests

--- src/test/java/staff/DeveloperTest.java
+++ src/test/java/staff/DeveloperTest.java
@@ -1,4 +1,48 @@
 package staff;
 
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Billy", 34567, 20000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Kev", developer.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals(12345, developer.getNiNumber(), 0.01);
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(35000.50, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        //WHEN
+        developer.raiseSalary(200);
+        //THEN
+        assertEquals(20200, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus(){
+        //WHEN
+        developer.payBonus();
+        //THEN
+        assertEquals(200, developer.payBonus(), 0.01);
+    }
 }
