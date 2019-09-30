1023f7fa60e2ae41a98b954a58b3d85d472b83e3
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-30 16:03:44 +0100

    Initial Commit

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,17 @@
+package staff;
+
+
+public class Employee {
+
+    private String name;
+    private Integer niNumber;
+    private double salary;
+
+    public Employee(String name, Integer niNumber, double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+
+}
